c2649e531176114e24e9f3410e7a1da46e450c7b
Tristan Chin <tristan.chin@chintristan.io>
2020-12-02 14:54:39 -0500

    indent d2c1

--- calendar/day 2/challenge 1/index.ts
+++ calendar/day 2/challenge 1/index.ts
@@ -8,38 +8,40 @@ try {
     const input = fs.readFileSync(path.join(__dirname, "input.txt"));
     const text = input.toString();
 
-    const valid = _(text).split("\n").reduce((acc, line) => {
-        const minStr = line.slice(0, line.indexOf("-"));
-        const min = Number(minStr);
+    const valid = _(text)
+        .split("\n")
+        .reduce((acc, line) => {
+            const minStr = line.slice(0, line.indexOf("-"));
+            const min = Number(minStr);
 
-        if (isNaN(min)) {
-            throw new Error(`min: ${min} is not a number`);
-        }
+            if (isNaN(min)) {
+                throw new Error(`min: ${min} is not a number`);
+            }
 
-        line = line.slice(minStr.length + 1);
+            line = line.slice(minStr.length + 1);
 
-        const maxStr = line.slice(0, line.indexOf(" "));
-        const max = Number(maxStr);
+            const maxStr = line.slice(0, line.indexOf(" "));
+            const max = Number(maxStr);
 
-        line = line.slice(maxStr.length + 1);
+            line = line.slice(maxStr.length + 1);
 
-        if (isNaN(max)) {
-            throw new Error(`max: ${max} is not a number`);
-        }
+            if (isNaN(max)) {
+                throw new Error(`max: ${max} is not a number`);
+            }
 
-        const char = line[0];
-        const password = line.slice(3);
+            const char = line[0];
+            const password = line.slice(3);
 
-        const count = _.countBy(password, (c) => c == char)["true"];
+            const count = _.countBy(password, (c) => c == char)["true"];
 
-        if (count >= min && count <= max) {
-            ++acc;
-        }
+            if (count >= min && count <= max) {
+                ++acc;
+            }
 
-        return acc;
-    }, 0);
+            return acc;
+        }, 0);
 
-    log.success(`Valid passwords: ${chalk.bold(valid)}`)
+    log.success(`Valid passwords: ${chalk.bold(valid)}`);
 } catch (e) {
     log.error(e);
 }
