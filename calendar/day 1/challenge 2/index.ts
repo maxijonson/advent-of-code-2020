7bb9bd587896576fdf57e3a5897eb873a989a7f8
Tristan Chin <tristan.chin@chintristan.io>
2020-12-02 14:21:30 -0500

    Renamed some stuff

--- calendar/day 1/challenge 2/index.ts
+++ calendar/day 1/challenge 2/index.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import _ from "lodash";
-import { log } from "../../utils";
+import { log } from "../../../utils";
 import chalk from "chalk";
 
 try {
